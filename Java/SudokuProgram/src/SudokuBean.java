c3caf3ff755d25daa75d8bd849d5cab13413c577
KYOUNGBEOM <112946948+KYOUNGBEOM@users.noreply.github.com>
2024-03-21 17:36:08 +0900

    Add files via upload

---
+++ Java/SudokuProgram/src/SudokuBean.java
@@ -0,0 +1,11 @@
+public class SudokuBean {
+	private Object [][] sudoku_puzzle = new Object[9][9];
+
+	public Object[][] getSudoku_puzzle() {
+		return sudoku_puzzle;
+	}
+
+	public void setSudoku_puzzle(Object[][] sudoku_puzzle) {
+		this.sudoku_puzzle = sudoku_puzzle;
+	}
+}
